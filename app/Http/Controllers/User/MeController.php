4264cd39f12b67369565fda25f2cc273e71df3ed
Syafiq Azizan <syafiqaizan85@gmail.com>
2020-09-18 22:39:14 +0800

    customize user resource

--- app/Http/Controllers/User/MeController.php
+++ app/Http/Controllers/User/MeController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 
 class MeController extends Controller
 {
   public function getMe()
   {
       if(auth()->check()){
-          return response()->json(["user" => auth()->user()], 200);
+        $user = auth()->user();
+        return new UserResource($user);
       }
 
       return response()->json(null, 401);
