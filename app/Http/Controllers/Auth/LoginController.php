6cd49be38d882a7555c68583c64ed568fdeda885
Syafiq Azizan <syafiqaizan85@gmail.com>
2020-09-18 22:26:42 +0800

    JWT token login api

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -21,20 +24,59 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function attemptLogin(Request $request)
     {
-        $this->middleware('guest')->except('logout');
+        // attempt to issue a token to the user based on the login credentials
+        $token = $this->guard()->attempt($this->credentials($request));
+
+        if( ! $token){
+            return false;
+        }
+
+        // get the authenticated user
+        $user = $this->guard()->user();
+
+        if($user instanceof MustVerifyEmail && ! $user->hasVerifiedEmail()){
+            return false;
+        }
+
+        // set the user's token
+        $this->guard()->setToken($token);
+
+        return true;
+    }
+
+    protected function sendLoginResponse(Request $request)
+    {
+        // check how many user attempts & clear
+        $this->clearLoginAttempts($request);
+
+        // get the tokem from the authentication guard (JWT)
+        $token = (string)$this->guard()->getToken();
+
+        // extract the expiry date of the token
+        $expiration = $this->guard()->getPayload()->get('exp');
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $expiration
+        ]);
+    }
+
+    protected function sendFailedLoginResponse()
+    {
+        $user = $this->guard()->user();
+
+        // if not verify email yet
+        if($user instanceof MustVerifyEmail && ! $user->hasVerifiedEmail()){
+            return response()->json(["errors" => [
+                "verification" => "You need to verify your email account"
+            ]]);
+        }
+
+        throw ValidationException::withMessages([
+            $this->username() => "Invalid credentials"
+        ]);
     }
 }
