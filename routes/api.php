d68aa8491104efb52ee101a205fe11002193b9e0
Syafiq Azizan <syafiqaizan85@gmail.com>
2020-09-18 18:37:48 +0800

    define public:api, auth:api and guest:api

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,23 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Public routes
+Route::get('me', 'User\MeController@getMe');
+
+// Route for authenticated users only
+Route::group(['middleware' => ['auth:api']], function(){
+    Route::post('logout', 'Auth\LoginController@logout');
+    Route::put('settings/profile', 'User\SettingsController@updateProfile');
+    Route::put('settings/password', 'User\SettingsController@updatePassword');
+});
+
+// Routes for guests users only
+Route::group(['middleware' => ['guest:api']], function(){
+    Route::post('register', 'Auth\RegisterController@register');
+    Route::post('verification/verify/{user}', 'Auth\VerificationController@verify')->name('verification.verify');
+    Route::post('verification/resend', 'Auth\VerificationController@resend');
+    Route::post('login', 'Auth\LoginController@login');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+});
