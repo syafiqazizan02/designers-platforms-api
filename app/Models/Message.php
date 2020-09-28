8e289b6986477c23938b63e7ca649a8a5a24589b
Syafiq Azizan <syafiqaizan85@gmail.com>
2020-09-29 01:55:00 +0800

    add access modifier for deleted messages

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -18,6 +18,19 @@ class Message extends Model
         'last_read'
     ];
 
+    public function getBodyAttribute($value)
+    {
+        if($this->trashed()){
+            if(!auth()->check()) return null;
+
+            return auth()->id() == $this->sender->id ?
+                    'You deleted this message' :
+                    // "{$this->sender->name} deleted this message";
+                    "This message was deleted";
+        }
+        return $value;
+    }
+
     public function chat()
     {
         return $this->belongsTo(Chat::class);
